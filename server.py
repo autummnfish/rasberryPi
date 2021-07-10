4b3e129527271782e3225c1da615ff1917a6d111
sanma-mod <mrstrandjpn@gmail.com>
2021-07-11 01:02:17 +0900

    とりあえず動く

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 import asyncio
 import websockets
+connection = set()
 
-async def echo(websocket,path):
-    while True:
+async def hello(websocket, path):
+    connection.add(websocket)
+    try:
         data = await websocket.recv()
-        print('Received')
-        await websocket.send(data)
-        
-# start_server = websockets.serve(echo, "0.0.0.0",3000)
-start_server = websockets.serve(echo, "localhost",3000)
+        print(data)
+        await asyncio.wait([ws.send({data}) for ws in connection])
+        await asyncio.sleep(10)
+    finally:
+        connection.remove(websocket)
 
-
-
-
+start_server = websockets.serve(hello, "0.0.0.0", 3000)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
